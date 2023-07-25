9fed96547cbac3bea4cb7b84c5c0e0d275eeeca4
JusticeOpara <justiceopara33@gmail.com>
2023-07-25 18:50:46 +0100

    made the app  moblie responsive

--- src/pages/VideoPlayer.jsx
+++ src/pages/VideoPlayer.jsx
@@ -5,8 +5,8 @@ export default function Videoplayer({productVideo}) {
     console.log(productVideo,"---videodfghdfhfdh")
     return (
         <>
-            <div className="flex w-full h-[400px] justify-center bg-red-200 rounded items-center">
-                <ReactPlayer className="w-[715px] max-md:w-[10%] h-[400px] flex-shrink-0 "  url={productVideo} />
+            <div className="flex w-full h-[400px] justify-center  max-sm:hidden rounded items-center">
+                <ReactPlayer className="w-[715px] smre:w-[100%] max-sm:w-[10px] h-[400px] flex-shrink-0 "  url={productVideo} />
             </div>
            
         </>
